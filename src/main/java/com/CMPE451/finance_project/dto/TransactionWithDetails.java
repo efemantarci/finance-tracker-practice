8dba2ee3618919b35dcce6c20955f8f302749a66
Efe Mantaroğlu <efemantaroglum@gmail.com>
2025-09-17 15:48:21 +0300

    feat: add getTransaction to the service

--- src/main/java/com/CMPE451/finance_project/dto/TransactionWithDetails.java
+++ src/main/java/com/CMPE451/finance_project/dto/TransactionWithDetails.java
@@ -1,4 +1,41 @@
 package com.CMPE451.finance_project.dto;
 
+import com.CMPE451.finance_project.model.Transaction;
+import com.CMPE451.finance_project.model.TransactionCategory;
+import com.CMPE451.finance_project.model.User;
+
 public class TransactionWithDetails {
+    private Transaction transaction;
+    private User user;
+    private TransactionCategory category;
+
+    public TransactionWithDetails(Transaction transaction, User user, TransactionCategory category) {
+        this.transaction = transaction;
+        this.user = user;
+        this.category = category;
+    }
+
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public TransactionCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(TransactionCategory category) {
+        this.category = category;
+    }
 }
--- src/main/java/com/CMPE451/finance_project/service/TransactionService.java
+++ src/main/java/com/CMPE451/finance_project/service/TransactionService.java
@@ -1,11 +1,42 @@
 package com.CMPE451.finance_project.service;
 
+import com.CMPE451.finance_project.dto.TransactionWithDetails;
+import com.CMPE451.finance_project.model.Transaction;
+import com.CMPE451.finance_project.model.TransactionCategory;
 import com.CMPE451.finance_project.model.User;
 import com.CMPE451.finance_project.repository.TransactionCategoryRepository;
 import com.CMPE451.finance_project.repository.TransactionRepository;
+import com.CMPE451.finance_project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TransactionService {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    TransactionCategoryRepository transactionCategoryRepository;
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    ResponseEntity<TransactionWithDetails> getTransaction(int transactionId){
+        Optional<Transaction> transactionOpt = transactionRepository.getTransaction(transactionId);
+        if(transactionOpt.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        Transaction t = transactionOpt.get();
+        Optional<User> userOpt = userRepository.getUser(t.getUser());
+        Optional<TransactionCategory> category = transactionCategoryRepository.getCategory(t.getCategoryId());
+        // Create response object
+        TransactionWithDetails res = new TransactionWithDetails(
+                t,
+                userOpt.orElse(null),
+                category.orElse(null)
+        );
+
+        return ResponseEntity.ok(res);
+    }
 }
